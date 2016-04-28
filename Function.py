be2dc1c7ef157c465756e0c4cd6c0355d0a6aa3b
moneyyshot <moneyyshot@gmail.com>
2016-04-27 23:49:53 -0400

    Update Function.py
    added some comments and a few lines of code

--- Function.py
+++ Function.py
@@ -1,1 +1,13 @@
 print ("Hello, World")
+
+userName = input ("What is your name? ")
+#this is a basic prompt which asks the user for their name
+
+if (userName > 4) {
+  print("What a nice name")
+  }
+  
+  else {
+    return("Your name is too short!")
+    }
+    #a simple conditional statement
