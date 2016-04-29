d38916c940492a2333f3da0ac7cc0a6484fe5519
Marlon Louis <Marlon Louis>
2016-04-29 08:54:22 -0400

    We should add this to the repository

---
+++ calculator.py
@@ -0,0 +1,11 @@
+userName = input("What is your name young man?")
+
+if (userName.length >= 3){
+    print("Hello my fello Americain!")
+}else{
+    
+    print("Get the hell out of here you asian loser")
+}
+
+
+x = "p"
